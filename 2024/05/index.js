b90facaf3392836f7be430d8072d69c1dad12bae
Ivan GaLa <github@laodesign.es>
2024-12-05 20:07:04 +0100

    Reto 5

---
+++ 2024/05/index.js
@@ -0,0 +1,87 @@
+/*
+Los elfos 🧝🧝‍♂️ de Santa Claus han encontrado un montón de botas mágicas desordenadas en el taller. Cada bota se describe por dos valores:
+
+type indica si es una bota izquierda (I) o derecha (R).
+size indica el tamaño de la bota.
+Tu tarea es ayudar a los elfos a emparejar todas las botas del mismo tamaño que tengan izquierda y derecha. Para ello, debes devolver una lista con los pares disponibles después de emparejar las botas.
+
+¡Ten en cuenta que puedes tener más de una zapatilla emparejada del mismo tamaño!
+
+const shoes = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 },
+  { type: 'R', size: 42 },
+  { type: 'I', size: 41 },
+  { type: 'I', size: 42 }
+]
+
+organizeShoes(shoes)
+// [38, 42]
+
+const shoes2 = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 },
+  { type: 'I', size: 38 },
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 }
+]
+// [38, 38]
+
+const shoes3 = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 36 },
+  { type: 'R', size: 42 },
+  { type: 'I', size: 41 },
+  { type: 'I', size: 43 }
+]
+
+organizeShoes(shoes3)
+// []
+*/
+
+function organizeShoes(shoes) {
+  const stock = {}
+  const result = []
+  for (const { type, size } of shoes) {
+    if (!stock[size]) stock[size] = { I: 0, R: 0 }
+    stock[size][type]++
+    if (stock[size]['I'] > 0 && stock[size]['R'] > 0) {
+      result.push(size)
+      stock[size]['I']--
+      stock[size]['R']--
+    }
+  }
+  return result;
+}
+
+const shoes = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 },
+  { type: 'R', size: 42 },
+  { type: 'I', size: 41 },
+  { type: 'I', size: 42 }
+]
+
+console.log(organizeShoes(shoes));
+// [38, 42]
+
+const shoes2 = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 },
+  { type: 'I', size: 38 },
+  { type: 'I', size: 38 },
+  { type: 'R', size: 38 }
+]
+console.log(organizeShoes(shoes2));
+// [38, 38]
+
+const shoes3 = [
+  { type: 'I', size: 38 },
+  { type: 'R', size: 36 },
+  { type: 'R', size: 42 },
+  { type: 'I', size: 41 },
+  { type: 'I', size: 43 }
+]
+
+console.log(organizeShoes(shoes3));
+// []
