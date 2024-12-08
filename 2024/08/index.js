e7412cdd36a6a8dff6b231003e2f5b74c01157ef
Ivan GaLa <github@laodesign.es>
2024-12-08 13:36:12 +0100

    Reto 8

---
+++ 2024/08/index.js
@@ -0,0 +1,63 @@
+/*
+¡Es hora de seleccionar a los renos más rápidos para los viajes de Santa! 🦌🎄
+Santa Claus ha organizado unas emocionantes carreras de renos para decidir cuáles están en mejor forma.
+
+Tu tarea es mostrar el progreso de cada reno en una pista de nieve en formato isométrico.
+
+La información que recibes:
+
+indices: Un array de enteros que representan el progreso de cada reno en la pista:
+0: El carril está vacío.
+Número positivo: La posición actual del reno desde el inicio de la pista.
+Número negativo: La posición actual del reno desde el final de la pista.
+length: La longitud de cada carril.
+Devuelve un string que represente la pista de la carrera:
+
+Cada carril tiene exactamente length posiciones llenas de nieve (~).
+Cada reno se representa con la letra r.
+Los carriles están numerados al final con /1, /2, etc.
+La vista es isométrica, por lo que los carriles inferiores están desplazados hacia la derecha.
+Ejemplos:
+*/
+
+
+function drawRace(indices, length) {
+  const nieve = '~'
+  const reno = 'r'
+  const espacio = ' '
+  let result = ''
+
+  for (let i = 0; i < indices.length; i++) {
+    const final = ` /${i + 1}\n`
+    let line = (new Array(length).fill(nieve).join(''))
+    const index = indices[i] < 0 ? line.length + indices[i] : indices[i]
+    if (indices[i] !== 0) line = line.substring(0, index) + reno + line.substring(index + 1)
+    line = line.padStart(length + indices.length - i - 1, espacio) + final
+    result += line
+  }
+
+  return result.slice(0, -1)
+}
+
+
+console.log(drawRace([0, 5, -3], 10));
+/*
+  ~~~~~~~~~~ /1
+ ~~~~~r~~~~ /2
+~~~~~~~r~~ /3
+*/
+
+console.log(drawRace([2, -1, 0, 5], 8));
+/*
+   ~~r~~~~~ /1
+  ~~~~~~~r /2
+ ~~~~~~~~ /3
+~~~~~r~~ /4
+*/
+
+console.log(drawRace([3, 7, -2], 12));
+/*
+  ~~~r~~~~~~~~ /1
+ ~~~~~~~~r~~~ /2
+~~~~~~~~~r~~ /3
+*/
