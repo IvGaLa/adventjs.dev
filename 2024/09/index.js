cad39c61c813ac8b09e3ebf6417eb0267e30f449
Ivan GaLa <github@laodesign.es>
2024-12-09 12:11:01 +0100

    Reto 9

---
+++ 2024/09/index.js
@@ -0,0 +1,90 @@
+/*
+
+Los elfos están jugando con un tren 🚂 mágico que transporta regalos. Este tren se mueve en un tablero representado por un array de strings.
+
+El tren está compuesto por una locomotora (@), seguida de sus vagones (o), y debe recoger frutas mágicas (*) que le sirve de combustible. El movimiento del tren sigue las siguientes reglas:
+
+Recibirás dos parámetros board y mov.
+
+board es un array de strings que representa el tablero:
+
+@ es la locomotora del tren.
+o son los vagones del tren.
+* es una fruta mágica.
+· son espacios vacíos.
+mov es un string que indica el próximo movimiento del tren desde la cabeza del tren @:
+
+'L': izquierda
+'R': derecha
+'U': arriba
+'D': abajo.
+Con esta información, debes devolver una cadena de texto:
+
+'crash': Si el tren choca contra los bordes del tablero o contra sí mismo.
+'eat': Si el tren recoge una fruta mágica (*).
+'none': Si avanza sin chocar ni recoger ninguna fruta mágica.
+Ejemplo:
+
+const board = ['·····', '*····', '@····', 'o····', 'o····']
+
+console.log(moveTrain(board, 'U'))
+// ➞ 'eat'
+// Porque el tren se mueve hacia arriba y encuentra una fruta mágica
+
+console.log(moveTrain(board, 'D'))
+// ➞ 'crash'
+// El tren se mueve hacia abajo y la cabeza se choca consigo mismo
+
+console.log(moveTrain(board, 'L'))
+// ➞ 'crash'
+// El tren se mueve a la izquierda y se choca contra la pared
+
+console.log(moveTrain(board, 'R'))
+// ➞ 'none'
+// El tren se mueve hacia derecha y hay un espacio vacío en la derecha
+
+
+*/
+
+function moveTrain(board, mov) {
+  const movimientos = {
+    'U': { x: 0, y: -1 },
+    'D': { x: 0, y: 1 },
+    'L': { x: -1, y: 0 },
+    'R': { x: 1, y: 0 }
+  };
+  const results = {
+    '·': 'none',
+    '*': 'eat',
+    'o': 'crash'
+  };
+
+  const y = board.findIndex(row => row.includes('@'));
+  const x = board[y].indexOf('@');
+
+  const newY = y + movimientos[mov].y;
+  const newX = x + movimientos[mov].x;
+
+  if (newY < 0 || newY >= board.length || newX < 0 || newX >= board[0].length) return 'crash';
+
+  return results[board[newY][newX]] || 'none';
+}
+
+
+const board = ['·····', '*····', '@····', 'o····', 'o····']
+
+console.log(moveTrain(board, 'U'))
+// ➞ 'eat'
+// Porque el tren se mueve hacia arriba y encuentra una fruta mágica
+
+console.log(moveTrain(board, 'D'))
+// ➞ 'crash'
+// El tren se mueve hacia abajo y la cabeza se choca consigo mismo
+
+console.log(moveTrain(board, 'L'))
+// ➞ 'crash'
+// El tren se mueve a la izquierda y se choca contra la pared
+
+console.log(moveTrain(board, 'R'))
+// ➞ 'none'
+// El tren se mueve hacia derecha y hay un espacio vacío en la derecha
